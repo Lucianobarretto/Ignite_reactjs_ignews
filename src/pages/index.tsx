31f4ced21025d2b50ee833f38f3f01fa1dabe8bb
Luciano Barreto <contato.lucianobarreto@gmail.com>
2021-04-06 17:21:54 -0300

    Static Site Generation (SSG)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
+
+// Três formas principais de fazer uma chamada API:
+//                                                 => Client-side
+//                                                 => Server-side Rendering (SSR)
+//                                                 => Static Site Generation (SSG)
 
 interface HomeProps {
   product: {
@@ -36,7 +41,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1IdJyIKYPE27EQqyuRBmSOmy')
 
   const product = {
@@ -50,6 +55,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
