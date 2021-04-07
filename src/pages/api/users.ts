38acfb87a2764eb8e6f02a84532fccbd628e9998
Luciano Barreto <contato.lucianobarreto@gmail.com>
2021-04-07 14:39:49 -0300

    API routes no Next.js

---
+++ src/pages/api/users.ts
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+  const users = [
+    { id: 1, name: 'Luciano' },
+    { id: 2, name: 'Ana Paula' },
+    { id: 3, name: 'Líria' },
+  ]
+
+  return response.json(users)
+}
+
+// Serverless
