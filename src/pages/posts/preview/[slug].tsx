2c0554532e48e7b4e7e2cf85ab24839d8fb76f16
Luciano Barreto <contato.lucianobarreto@gmail.com>
2021-08-05 20:56:56 -0300

    Gerando previews estáticos

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -56,7 +56,7 @@ export default function PostPreview({ post }: PostPreviewProps) {
   )
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticProps = async () => {
   return {
     paths:[],
     fallback: 'blocking'
@@ -84,6 +84,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   return {
     props: {
       post,
-    }
+    },
+    redirect: 60 * 30, // 30 minutes
   }
 }
